fa38f561509a1bd844dc8b9a3a0f979af13f1697
DESKTOP-M47AOQI\ductt <ductt4@topica.edu.vn>
2018-11-22 10:52:55 +0700

    add-migration, remove foreign-key

---
+++ Storage.Api/Migrations/20181121155602_Remove_roleId.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Storage.Api.Migrations
+{
+    public partial class Remove_roleId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
