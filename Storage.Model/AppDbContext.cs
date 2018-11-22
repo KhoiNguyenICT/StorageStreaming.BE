bd7fb0cb201871e97f8a9808c4b480ef62ca2ace
DESKTOP-M47AOQI\ductt <ductt4@topica.edu.vn>
2018-11-22 10:55:08 +0700

    remove foreign key

--- Storage.Model/AppDbContext.cs
+++ Storage.Model/AppDbContext.cs
@@ -2,14 +2,12 @@
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Storage.Common.Interfaces;
-using Storage.Model.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Storage.Common.Interfaces;
 using Storage.Model.Entities;
-
+using Storage.Model.Entities.temp;
 
 namespace Storage.Model
 {
@@ -30,36 +28,30 @@ namespace Storage.Model
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Entities.temp.RolePermission>()
-       .HasKey(c => new { c.RoleId, c.PermissionId });
+            modelBuilder.Entity<RolePermission>()
+                .HasOne(bc => bc.Role)
+                .WithMany(b => b.Permissions)
+                .HasForeignKey(bc => bc.RoleId);
 
-            modelBuilder.Entity<Entities.temp.RolePermission>()
-        .HasOne(bc => bc.Role)
-        .WithMany(b => b.Permissions)
-        .HasForeignKey(bc => bc.RoleId);
-
-            modelBuilder.Entity<Entities.temp.RolePermission>()
-        .HasOne(bc => bc.Permission)
-        .WithMany(b => b.Roles)
-        .HasForeignKey(bc => bc.PermissionId);
+            modelBuilder.Entity<RolePermission>()
+                .HasOne(bc => bc.Permission)
+                .WithMany(b => b.Roles)
+                .HasForeignKey(bc => bc.PermissionId);
 
             base.OnModelCreating(modelBuilder);
         }
 
         public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
         {
-            var modified = ChangeTracker.Entries().Where(e => e.State == EntityState.Modified || e.State == EntityState.Added);
-            foreach (EntityEntry item in modified)
-            {
+            var modified = ChangeTracker.Entries()
+                .Where(e => e.State == EntityState.Modified || e.State == EntityState.Added);
+            foreach (var item in modified)
                 if (item.Entity is IEntity changedOrAddedItem)
                 {
-                    if (item.State == EntityState.Added)
-                    {
-                        changedOrAddedItem.CreatedDate = DateTime.Now;
-                    }
+                    if (item.State == EntityState.Added) changedOrAddedItem.CreatedDate = DateTime.Now;
                     changedOrAddedItem.UpdatedDate = DateTime.Now;
                 }
-            }
+
             return base.SaveChangesAsync(cancellationToken);
         }
     }
